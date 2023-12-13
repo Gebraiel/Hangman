b585be470b4d1ab70fa3ca3d381fb1e1089b83df
Gebraiel <67439330+Gebraiel@users.noreply.github.com>
2023-12-13 14:04:07 +0200

    .

---
+++ popup.js
@@ -0,0 +1,37 @@
+
+
+  function createPopUp(word,meaning){
+    let popup = document.createElement('div');
+    popup.className = 'popup';
+  
+    let message = document.createElement('p');
+    message.className = 'message';
+    let h1 = document.createElement('h1');
+    let welcomeTextNode = document.createTextNode(word);
+    h1.appendChild(welcomeTextNode);
+    let innerP = document.createElement('p');
+    let paragraphTextNode = document.createTextNode(meaning);
+    innerP.appendChild(paragraphTextNode); 
+    message.appendChild(h1);
+    message.appendChild(innerP);
+  
+    let closeButton = document.createElement('button');
+    closeButton.setAttribute('class','close-button');
+    let closeButtonTextNode = document.createTextNode("X");
+    closeButton.appendChild(closeButtonTextNode);
+  
+    popup.appendChild(message);
+    popup.appendChild(closeButton);
+  
+    let overlay = document.createElement('div');
+    overlay.classList.add('overlay');
+  
+    document.body.prepend(overlay);
+    document.body.prepend(popup);
+  
+    closeButton.addEventListener('click',(e)=>{
+        
+        popup.remove();
+        overlay.remove();
+    });
+  }
